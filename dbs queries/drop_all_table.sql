f8618b722b6b9013f70c0701abdbdb123338d373
TuNguyen2212 <hoangtu22122004@gmail.com>
2024-12-02 16:55:47 +0700

    Finish drop_all_table

---
+++ dbs queries/drop_all_table.sql
@@ -0,0 +1,33 @@
+DROP TABLE ButToanThu;
+
+DROP TABLE ButToanChi;
+
+DROP TABLE ButToan;
+
+DROP TABLE QuanLyCaLam;
+
+DROP TABLE CaLam;
+
+DROP TABLE DonHang_Taicho;
+
+DROP TABLE DonHang_Tructuyen;
+
+DROP TABLE Quan_ly_mon_an_trong_DonHang;
+
+DROP TABLE DonHang;
+
+DROP TABLE MonAn_TrongNgay;
+
+DROP TABLE MonAn;
+
+DROP TABLE Vi_pham;
+
+DROP TABLE KhachHang;
+
+DROP TABLE NhanVien;
+
+DROP TABLE CongTyThucPham;
+
+DROP TABLE CongTyVanChuyen;
+
+DROP TABLE Ben_Nhan;
