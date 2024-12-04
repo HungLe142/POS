a6e26c929366c45060f6493f88965bb852eab0cc
Hung Le <lethaihung142@gmail.com>
2024-12-04 08:04:39 +0700

    show data of staffs, sort staff

--- App/views/view4.py
+++ App/views/view4.py
@@ -1,5 +1,9 @@
 # view Nhân viên
 import tkinter as tk
+from tkinter import ttk
+from controllers.view4_controller import get_staff
+from views.vỉew1 import sort_table
+
 
 def show_view4(parent):
 
@@ -7,13 +11,37 @@ def show_view4(parent):
     parent.label = tk.Label(parent.content_frame, text="Nhân viên")
     parent.label.pack()
 
-    parent.data_listbox = tk.Listbox(parent.content_frame)
-    parent.data_listbox.pack(fill=tk.BOTH, expand=True)
+    # Frame để thêm dữ liệu
+    #insert_frame = create_insert_frame(parent)
+    #insert_frame.pack(fill=tk.X)
 
-    """
-    Hiển thị View2 trong khung nội dung của lớp chính.
-    """
-    # Giả sử nội dung của view 2 là một danh sách các đơn hàng
-    orders = ["Staff 1", "Staff 2", "Staff 3"]
-    for order in orders:
-        parent.data_listbox.insert(tk.END, order)
+    # Bảng dữ liệu nhân viên
+    staff_table = create_staff_table(parent.content_frame)
+    staff_table.pack(fill=tk.BOTH, expand=True)
+
+    if parent.staff_buff is None:
+        parent.staff_buff = get_staff()
+
+    if parent.staff_buff:
+        for staff in parent.staff_buff:
+            data = (staff[0], staff[1], staff[2], staff[4], staff[5])
+            staff_table.insert('', 'end', values=data)
+
+def create_insert_frame(parent):
+    pass
+
+def create_staff_table(parent):
+    staff_table = ttk.Treeview(parent, columns = ('ID', 'Tên', 'SĐT', 'Chức vụ', 'Hệ số lương'), show = 'headings', height=8)
+    staff_table.heading('ID', text = 'ID')
+    staff_table.heading('Tên', text = 'Tên')
+    staff_table.heading('SĐT', text = 'SĐT')
+    staff_table.heading('Chức vụ', text = 'Chức vụ')
+    staff_table.heading('Hệ số lương', text = 'Hệ số lương', command=lambda: sort_table(staff_table, 'Hệ số lương', False))
+
+    staff_table.column('ID', width=82) 
+    staff_table.column('Tên', width=82) 
+    staff_table.column('SĐT', width=82) 
+    staff_table.column('Chức vụ', width=82)
+    staff_table.column('Hệ số lương', width=82)
+
+    return staff_table
