e2bb8fefa4fbe30ad50ea8980b1e046ac27682e9
TuNguyen2212 <hoangtu22122004@gmail.com>
2024-12-05 17:42:12 +0700

    delete_nhanvien_trigger

---
+++ 1.2/2/delete_nhanvien_trigger.sql
@@ -0,0 +1,24 @@
+CREATE TRIGGER trg_NhanVien_CheckState
+ON NhanVien
+INSTEAD OF DELETE
+AS
+BEGIN
+    -- Kiểm tra ràng buộc : Chỉ được xóa nhân viên đã nghỉ làm 
+    -- và ca làm cuối cùng của nhân viên đó cách hiện tại ít nhất là 6 tháng
+    IF EXISTS (
+        SELECT 1
+        FROM ((deleted i
+        JOIN QuanLyCaLam q ON i.ID_NhanVien = q.ID_NhanVien) JOIN CaLam c ON q.ID_calam = c.ID_calam)
+        WHERE ( i.Trang_thai IN ('Tich cuc', 'Binh thuong', 'Khong tich cuc') OR 
+        DATEDIFF(month, GETDATE(), c.Ngay)<6 )
+    )
+    BEGIN
+        RAISERROR ('Nhân viên được xóa phải là nhân viên đã nghỉ việc ít nhất 6 tháng.', 16, 1);
+        ROLLBACK;
+        RETURN;
+    END
+
+    -- Thêm hoặc cập nhật dữ liệu vào bảng QuanLyCaLam
+    DELETE FROM NhanVien
+    WHERE ID_NhanVien IN (SELECT ID_NhanVien FROM deleted)
+END;
