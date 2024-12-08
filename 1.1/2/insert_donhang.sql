f8ec59b4aa0f28d5d483f161e0af802082a7ac0c
Hung Le <lethaihung142@gmail.com>
2024-12-09 01:57:28 +0700

    bổ sung file thêm đơn hàng (Đã tạo data để test, nên xóa trước khi tạo raw data có ý nghĩa)

---
+++ 1.1/2/insert_donhang.sql
@@ -0,0 +1,13 @@
+INSERT INTO DonHang
+( 
+ [ID_don_hang], [ID_NhanVien], [Trang_thai], [Ghi_chu_trang_thai], [Gio_tao_don],[Tong_tien] 
+)
+VALUES
+/**/
+( 
+ 'OD000005', 'NV000001', 'Pending', 'Cho xu ly', '00:00:00.0000000', 100
+),
+
+( 
+ 'ODS000000', 'NV000001', 'Pending', 'Cho xu ly', '00:00:00.0000000', 100
+)
