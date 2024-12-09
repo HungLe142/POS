da8407a47aa42f6e852f13988afd7d75dd76d2a3
TuNguyen2212 <hoangtu22122004@gmail.com>
2024-12-09 23:51:13 +0700

    add table showing in trigger don hang

--- 1.2/2/donhang_trigger_testcase.sql
+++ 1.2/2/donhang_trigger_testcase.sql
@@ -1,3 +1,5 @@
+
+
 INSERT INTO DonHang
 ( 
  [ID_don_hang], [SDT_KhachHang], [ID_NhanVien],
@@ -8,6 +10,10 @@ VALUES
 ( 'OD100001', '0948315789', 'NV000001',
   'Success', 'Thanh cong','12:00:00',
   NULL,NULL,0)
+
+
+SELECT * FROM DonHang
+
 GO
 
 INSERT INTO DonHang
@@ -20,18 +26,22 @@ VALUES
 ( 'OD100001', '0948315789', 'NV000001',
   'Pending', 'Cho xu ly','12:00:00',
   NULL,NULL,0)
+SELECT * FROM DonHang
 GO
 
 UPDATE DonHang SET Trang_thai = 'Success', 
                Ghi_chu_trang_thai = 'Thanh cong' 
                WHERE ID_don_hang = 'OD100001'
+SELECT * FROM DonHang
 GO
 
 UPDATE DonHang SET Trang_thai = 'Not_paid', 
                Ghi_chu_trang_thai = 'Da nhan don (chua thanh toan)' 
                WHERE ID_don_hang = 'OD100001'
+SELECT * FROM DonHang
 GO
 
 DELETE FROM DonHang
 WHERE ID_don_hang = 'OD100001'
+SELECT * FROM DonHang
 GO
