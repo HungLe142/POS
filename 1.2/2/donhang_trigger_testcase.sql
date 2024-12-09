e7bf2343b98455373c9e9b98d68ad3bfaa92b267
TuNguyen2212 <hoangtu22122004@gmail.com>
2024-12-09 22:51:38 +0700

    finish all 1.2.2. and 1.2.4

--- 1.2/2/donhang_trigger_testcase.sql
+++ 1.2/2/donhang_trigger_testcase.sql
@@ -5,7 +5,7 @@ INSERT INTO DonHang
  [Gio_hoan_thanh], [Ghi_chu_don_hang], [Tong_tien]
 )
 VALUES
-( 'OD000001', '0948315789', 'NV000001',
+( 'OD100001', '0948315789', 'NV000001',
   'Success', 'Thanh cong','12:00:00',
   NULL,NULL,0)
 GO
@@ -17,21 +17,21 @@ INSERT INTO DonHang
  [Gio_hoan_thanh], [Ghi_chu_don_hang], [Tong_tien]
 )
 VALUES
-( 'OD000001', '0948315789', 'NV000001',
+( 'OD100001', '0948315789', 'NV000001',
   'Pending', 'Cho xu ly','12:00:00',
   NULL,NULL,0)
 GO
 
 UPDATE DonHang SET Trang_thai = 'Success', 
                Ghi_chu_trang_thai = 'Thanh cong' 
-               WHERE ID_don_hang = 'OD000001'
+               WHERE ID_don_hang = 'OD100001'
 GO
 
 UPDATE DonHang SET Trang_thai = 'Not_paid', 
                Ghi_chu_trang_thai = 'Da nhan don (chua thanh toan)' 
-               WHERE ID_don_hang = 'OD000001'
+               WHERE ID_don_hang = 'OD100001'
 GO
 
 DELETE FROM DonHang
-WHERE ID_don_hang = 'OD000001'
+WHERE ID_don_hang = 'OD100001'
 GO
