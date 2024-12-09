8bf0217e0459eb08bbe1fc7bfcd74b80cee1e463
Hung Le <lethaihung142@gmail.com>
2024-12-09 17:02:23 +0700

    close order ( ... -> success)

--- App/controllers/view2_controller.py
+++ App/controllers/view2_controller.py
@@ -1,8 +1,14 @@
 from models.database import fetch_orders
 from models.database import change_order_detail
+from models.database import change_order_status
+
 
 def get_orders():
     return fetch_orders()
 
 def update_order(main_view):
     return change_order_detail(main_view)
+
+# Use for close order
+def update_order1(main_view):
+    return change_order_status(main_view)
