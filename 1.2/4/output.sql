ada424909c39bdda9e544400135f46bca25f0fb9
TuNguyen2212 <hoangtu22122004@gmail.com>
2024-12-09 21:16:24 +0700

    Testcase result added for 1.2.4

--- 1.2/4/output.sql
+++ 1.2/4/output.sql
@@ -1,8 +1,18 @@
 DECLARE @res1 AS BIT;
--- EXEC @res1 = dbo.Kiem_Tra_Ngay_Ban @Ngayban = '2022-02-22'
--- EXEC @res1 = dbo.Kiem_Tra_Ngay_Ban @Ngayban = '2022-02-23'
-EXEC @res1 = dbo.Kiem_Tra_Ngay_Ban @Ngayban = '2022-02-24'
+DECLARE @res2 AS BIT;
+DECLARE @res3 AS BIT;
+EXEC @res1 = dbo.Kiem_Tra_Ngay_Ban @Ngayban = '2022-02-22'
+EXEC @res2 = dbo.Kiem_Tra_Ngay_Ban @Ngayban = '2022-02-23'
+EXEC @res3 = dbo.Kiem_Tra_Ngay_Ban @Ngayban = '2022-02-24'
 IF @res1 = 0
-PRINT 'Ngay ban that bai'
+PRINT 'Ngay ban 1 that bai'
 ELSE
-PRINT 'Ngay ban thanh cong'
+PRINT 'Ngay ban 1 thanh cong'
+IF @res2 = 0
+PRINT 'Ngay ban 2 that bai'
+ELSE
+PRINT 'Ngay ban 2 thanh cong'
+IF @res3 = 0
+PRINT 'Ngay ban 3 that bai'
+ELSE
+PRINT 'Ngay ban 3 thanh cong'
