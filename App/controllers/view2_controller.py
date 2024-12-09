2ec1397a527ec74ef848fbc9b2782f001dac180d
Hung Le <lethaihung142@gmail.com>
2024-12-09 20:51:50 +0700

    cancel order

--- App/controllers/view2_controller.py
+++ App/controllers/view2_controller.py
@@ -1,6 +1,4 @@
-from models.database import fetch_orders
-from models.database import change_order_detail
-from models.database import change_order_status
+from models.database import fetch_orders, change_order_detail, change_order_status, cancel_order
 
 
 def get_orders():
@@ -12,3 +10,6 @@ def update_order(main_view):
 # Use for close order
 def update_order1(main_view):
     return change_order_status(main_view)
+
+def cancel_the_order(main_view):
+    return cancel_order(main_view)
