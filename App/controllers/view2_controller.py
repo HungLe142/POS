6c540cb48c56a89d34862215a3fe5bee4647243a
Hung Le <lethaihung142@gmail.com>
2024-12-09 15:31:24 +0700

    add update order routes

--- App/controllers/view2_controller.py
+++ App/controllers/view2_controller.py
@@ -1,4 +1,8 @@
 from models.database import fetch_orders
+from models.database import change_order_detail
 
 def get_orders():
     return fetch_orders()
+
+def update_order(main_view):
+    return change_order_detail(main_view)
